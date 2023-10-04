7ae3981c8170cd12604eface8c2a7f7de42a0a19
Dwayna Harris <dwaynaharris@gmail.com>
2023-10-04 17:29:17 -0400

    Learn to use the key prop and change list of values to components

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,14 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+  const list = projects.map((item) => {
+    return <ProjectItem key={item.id} name={item.name} about={item.about} technologies={item.technologies} />
+  })
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{list}</div>
     </div>
   );
 }
